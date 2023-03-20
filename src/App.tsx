23095ea0481148680f492b2e5b475d9ca563a9ce
MichaillYasiuk <110971911+MichaillYasiuk@users.noreply.github.com>
2023-03-20 15:30:59 +0300

    del App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './router/router';
+import { router } from './routes/router';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
 

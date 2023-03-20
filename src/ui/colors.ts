c9e9dacc376b60d237def284e8fa2752e9311133
MichaillYasiuk <110971911+MichaillYasiuk@users.noreply.github.com>
2023-03-20 15:35:52 +0300

    add colors media

---
+++ src/ui/colors.ts
@@ -0,0 +1,13 @@
+export enum Color {
+  Primary = 'var(--primary)',
+  PrimaryLight = 'var(--primary-light)',
+  Secondary = 'var(--secondary)',
+  White = 'var(--white)',
+  Red = 'var(--red)',
+  Blue = 'var(--blue)',
+  Green = 'var(--green)',
+  Orange = 'var(--orange)',
+  Purple = 'var(--purple)',
+  Gray = 'var(--gray)',
+  Light = 'var(--light)',
+}

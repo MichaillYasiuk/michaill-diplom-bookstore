dba51e90c4008565e940754a729424c7bd1c9fac
MichaillYasiuk <110971911+MichaillYasiuk@users.noreply.github.com>
2023-03-25 16:12:50 +0300

    add firebase,routes,typogr

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { getAuth } from 'firebase/app';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_Key,
+  authDomain: process.env.REACT_APP_Aauth_Domain,
+  projectId: process.env.REACT_APP_Project_Id,
+  storageBucket: process.env.REACT_APP_Storage_Bucket,
+  messagingSenderId: process.env.REACT_APP_Messaging_SenderId,
+  appId: process.env.REACT_APP_App_Id,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
---
+++ src/routes/router.tsx
@@ -0,0 +1,46 @@
+import { Registration } from 'components/molecules/Registration/Registration';
+import { RequareAuth } from 'components/molecules/RequareAuth/RequareAuth';
+import {
+  MainPage,
+  SearchPage,
+  DetailsPage,
+  SignInPage,
+  SignUpPage,
+  ResetPasswordPage,
+  NotFoundPage,
+  FavoritesPage,
+  CartPage,
+  AccountPage,
+} from 'pages';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
+import { MainTemplate } from 'templates/MainTemplate';
+import { ROUTE } from './routes';
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path={ROUTE.MAIN} element={<MainTemplate />}>
+      <Route index element={<MainPage />} />
+
+      <Route path={ROUTE.SEARCH} element={<SearchPage />} />
+      <Route path={ROUTE.DETAILS} element={<DetailsPage />} />
+
+      <Route element={<Registration />}>
+        <Route path={ROUTE.SIGN_IN} element={<SignInPage />} />
+        <Route path={ROUTE.SIGN_UP} element={<SignUpPage />} />
+      </Route>
+
+      <Route path={ROUTE.RESET_PASSWORD} element={<ResetPasswordPage />} />
+      <Route path={ROUTE.NOT_FOUND} element={<NotFoundPage />} />
+
+      <Route element={<RequareAuth />}>
+        <Route path={ROUTE.FAVORITES} element={<FavoritesPage />} />
+        <Route path={ROUTE.CART} element={<CartPage />} />
+        <Route path={ROUTE.ACCOUNT} element={<AccountPage />} />
+      </Route>
+    </Route>
+  )
+);
